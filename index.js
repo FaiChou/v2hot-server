a1aebc1379bb5e0b4a1948a4377b64d0a622f520
FaiChou <faichou.zh@gmail.com>
2019-07-31 17:16:04 +0800

    change post to get

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const v2Client = restifyClient.createJsonClient({ url: 'https://www.v2ex.com' })
 const ninjaClient = restifyClient.createJsonClient({ url: 'http://statistics.pandadastudio.com' })
 
 server.pre(restify.plugins.pre.userAgentConnection());
-
+server.use(restify.plugins.bodyParser())
 server.use(
   function crossOrigin(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
@@ -16,7 +16,6 @@ server.use(
   }
 )
 
-server.use(restify.plugins.bodyParser())
 
 server.get('/v2hot', (req, res, next) => {
   v2Client.get('/api/topics/hot.json', (err, _req, _res, obj) => {
@@ -30,11 +29,8 @@ server.post('/tmp', (req, res, next) => {
   next()
 })
 
-server.post('/ninja', (req, res, next) => {
-  if (!req.is('application/json')) {
-    return next(new errors.InvalidContentError("Expects 'application/json'"))
-  }
-  const { uid, code } = req.body
+server.get('/ninja/:uid/:code', (req, res, next) => {
+  const { uid, code } = req.params
   ninjaClient.get(`/player/giftCode?uid=${uid}&code=${code}`, (err, _req, _res, obj) => {
     res.send(obj)
     next()
